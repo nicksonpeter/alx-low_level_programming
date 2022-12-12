0f8aa83e0679ef38d57466b8c346b7961c2e0b21
nicksonpeter <nickrocky5200@gmail.com>
2022-12-12 11:34:06 -0800

    done with mandatory task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,14 @@ int main(void)
 {
 	int numbers;
 
-	for (numbers = 48; numbers = 58; numbers++)
+	for (numbers = 48; numbers < 58; numbers++)
 	{
 		putchar(numbers);
-		putchar(',');
-		putchar(' ');
+		if (numbers != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
