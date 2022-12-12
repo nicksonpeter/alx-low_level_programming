5b5aaacc585fdaa623fee2d9c679713d7aedd118
nicksonpeter <nickrocky5200@gmail.com>
2022-12-12 08:15:49 -0800

    adding

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - alphabet exceptions
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha != 'e' && alpha != 'q')
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
