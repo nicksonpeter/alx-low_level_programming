4f14f227cb9ec057aba11daa7ad3b30a31275134
nicksonpeter <nickrocky5200@gmail.com>
2022-12-14 00:22:05 -0800

    adding task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - checking for lowercase
+ *
+ * Return: value 0 (Success)
+ */
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
