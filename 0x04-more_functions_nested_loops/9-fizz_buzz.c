60821e19878f33e75c8b581ea5a6ca8476375b27
nicksonpeter <nickrocky5200@gmail.com>
2022-12-17 06:11:40 -0800

    adding task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,35 +16,23 @@ int main(void)
 		if (num % 3 == 0 && num % 5 == 0)
 		{
 			if (num < 100)
-			{
 				printf("%s ", fizzbuzz);
-			}
 			else
-			{
 				printf("%s", fizzbuzz);
-			}
 		}
 		else if (num % 5 == 0)
 		{
 			if (num < 100)
-			{
 				printf("%s ", buzz);
-			}
 			else
-			{
 				printf("%s", buzz);
-			}
 		}
 		else if (num % 3 == 0)
 		{
 			if (num < 100)
-			{
 				printf("%s ", fizz);
-			}
 			else
-			{
 				printf("%s", fizz);
-			}
 		}
 		else
 		{
