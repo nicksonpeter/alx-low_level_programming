8e68ee48afb7abaf8ca6d376d4007772515102f1
nicksonpeter <nickrocky5200@gmail.com>
2022-12-19 13:53:12 -0800

    adding task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - printing the string in reverse
+ * @s: the string value
+ * Return: Output
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		else
+			count++;
+	}
+	
+	for (count--; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+
+	_putchar('\n');
+}
