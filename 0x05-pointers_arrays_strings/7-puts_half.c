00ef86af68af682429e0f57032c2444be0264bf5
nicksonpeter <nickrocky5200@gmail.com>
2022-12-20 11:53:18 -0800

    adding task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,14 +8,19 @@ void puts_half(char *str)
 {
 	int count = 0, var, val;
 
-	while (str[count] != '\0')
+	for (var = 0; str[var] != '\0': var++)
 	{
 		count++;
 	}
 
 	val = (count / 2);
 
-	for (var = val; var < count; var++)
+	if ((count % 2) == 1)
+	{
+		val = ((count + 1) / 2);
+	}
+
+	for (var = val; str[var] != '\0'; var++)
 	{
 		_putchar(str[var]);
 	}
