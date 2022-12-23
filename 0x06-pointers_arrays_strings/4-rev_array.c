399206bac181dddc61bf34a198b4c5b86ce36485
nicksonpeter <nickrocky5200@gmail.com>
2022-12-23 12:39:58 -0800

    adding task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reversing of array of integers
+ * @a: input value
+ * @n: input value
+ * Return: success value
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
+}
