f37996a1f08ffd49b0704919acc461098e1545be
nicksonpeter <nickrocky5200@gmail.com>
2023-01-07 06:43:04 -0800

    adding files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - printing its name
+ * @argc: length of argv array
+ * @argv: array containing arguments
+ */
+int main(int argc, char *argv[])
+{
+	puts(argv[argc - 1]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+the argc and argv
