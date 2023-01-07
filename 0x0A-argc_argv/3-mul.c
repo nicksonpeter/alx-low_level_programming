8ed349adbaa8ec53444538e403aead579ccfae55
nicksonpeter <nickrocky5200@gmail.com>
2023-01-07 07:27:58 -0800

    adding files

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all agruments
+ * @argc: length of argv array
+ * @argv: contains atguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int val = argc -1;
+
+	if (val != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
